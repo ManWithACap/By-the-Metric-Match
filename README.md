67a1abab21460066635739d05dbb2fb5057d163c
ManWithACap <scottb03202004@gmail.com>
2024-01-20 04:24:55 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# By the Metric Match
+## A game data tracker for your board/card/video games!
---
+++ main.py
@@ -0,0 +1,2 @@
+# Welcome to the main entrypoint of the app!
+# Enjoy your stay and grab a byte to eat! Make yourself at home!
